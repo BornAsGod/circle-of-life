dfb96af18e6e0bce7bb28f1619b6d81a1e7f820b
FadiElWash <fadi.nahhas1@gmail.com>
2021-02-27 16:40:50 +0100

    NavMesh/AI setup

---
+++ Assets/Scripts/BasePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasePlayer
+{
+    public float heatlh = 100f;
+    public float basicDamage = 10f;
+    public float specialDamage = 25f;
+    public float basicAttackRange;
+    public float specialAttackRange;
+    public GameObject favoriteFood;
+    public NavMeshAgent agent;
+
+    public IEnumerator move(Vector3 position)
+    {
+        agent.destination = position;
+        yield return new WaitForFixedUpdate();
+    }
+    public IEnumerator basicAttack(float attackRange)
+    {
+        yield return new WaitForFixedUpdate();
+    }    
+    public IEnumerator specialAttack(float attackRange)
+    {
+        yield return new WaitForFixedUpdate();
+    }
+}
---
+++ Assets/Scripts/MovementTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementTest : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+                Debug.Log(hit.point);
+            }
+        }
+    }
+}
