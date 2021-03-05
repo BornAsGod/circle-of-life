b9c91d7f9d15475d238580aaf7b8134660f45cfc
Valentin Ivanov <37935989+BornAsGod@users.noreply.github.com>
2021-03-05 20:51:09 +0100

    FoodSpawner
    randomly picks a child of the game object "Mid" which is the middle area and spawn "food" above it. It has 2 variables one for maximum amount of food on the map and one for current amount of food on the map. Those variables are for when player collides with food another one will be spawned.

---
+++ Assets/Scripts/food/foodSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodSpawner : MonoBehaviour
+{
+
+    public GameObject Food;
+    public Transform Mid;
+    public int Maxfood;
+    public int FoodSpawned = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawningFood();
+    }
+    public void SpawningFood()
+    {
+        int randomNum = Random.Range(0, Mid.childCount);
+        if (Maxfood>FoodSpawned)
+        {
+            Instantiate(Food, Mid.transform.GetChild(randomNum).position + (transform.up * 10), Quaternion.identity);
+            FoodSpawned++;
+        }
+
+
+    }
+
+
+}
+
+
+
