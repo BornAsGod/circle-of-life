d1e2b0e3cbd9ae399c9e1dcb21eb34f2b624f5c0
FadiElWash <fadi.nahhas1@gmail.com>
2021-03-06 13:37:53 +0100

    Update 2
    Added food assets
    more functions

--- Assets/Scripts/BasePlayer.cs
+++ Assets/Scripts/BasePlayer.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 
 public class BasePlayer
 {
-    public float heatlh = 100f;
+    public float health = 100f;
     public float basicDamage = 10f;
     public float specialDamage = 25f;
     public float basicAttackRange;
@@ -13,17 +14,29 @@ public class BasePlayer
     public GameObject favoriteFood;
     public NavMeshAgent agent;
 
-    public IEnumerator move(Vector3 position)
+    public IEnumerator Move(Vector3 position)
     {
         agent.destination = position;
         yield return new WaitForFixedUpdate();
     }
-    public IEnumerator basicAttack(float attackRange)
+    public IEnumerator BasicAttack(float attackRange)
     {
         yield return new WaitForFixedUpdate();
     }    
-    public IEnumerator specialAttack(float attackRange)
+    public IEnumerator SpecialAttack(float attackRange)
     {
+        yield return new WaitForFixedUpdate();
+    }
+
+    public IEnumerator TakeDamage(float damage)
+    {
+        health -= damage;
+        yield return new WaitForFixedUpdate();
+    }
+
+    public IEnumerator GetHealth(float healthAmount)
+    {
+        health += healthAmount;
         yield return new WaitForFixedUpdate();
     }
 }
