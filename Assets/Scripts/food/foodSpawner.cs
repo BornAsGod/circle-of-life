3e22bcc604c61bca0e5d223c666b302b4f5d53b7
Valentin Ivanov <37935989+BornAsGod@users.noreply.github.com>
2021-03-11 13:48:37 +0100

    Food spawn system on all the biomes

--- Assets/Scripts/food/foodSpawner.cs
+++ Assets/Scripts/food/foodSpawner.cs
@@ -6,9 +6,25 @@ public class foodSpawner : MonoBehaviour
 {
 
     public GameObject Food;
+
     public Transform Mid;
+    public Transform grass;
+    public Transform sand;
+    public Transform water;
+    public Transform dirt;
+    public Transform sandstone;
+
     public int Maxfood;
     public int FoodSpawned = 0;
+    
+    int RandomFood;
+    int RandomBiome;
+
+    public GameObject[] spawnFood;
+    GameObject currentFood;
+
+    public GameObject[] spawnBiome;
+    GameObject currentBiome;
 
 
     // Start is called before the first frame update
@@ -22,20 +38,46 @@ public class foodSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpawningFood();
+        SpawningFoodGrass();
     }
-    public void SpawningFood()
+    public void SpawningFoodMid()
     {
         int randomNum = Random.Range(0, Mid.childCount);
+        PickFood();
         if (Maxfood>FoodSpawned)
         {
-            Instantiate(Food, Mid.transform.GetChild(randomNum).position + (transform.up * 10), Quaternion.identity);
+            Instantiate(currentFood, Mid.transform.GetChild(randomNum).position + (transform.up * 8.5f), Quaternion.identity);
             FoodSpawned++;
         }
 
 
     }
+    public void SpawningFoodGrass()
+    {
+        
+        PickFood();
+        PickBiome();
+        int randomChild = Random.Range(0, currentBiome.transform.childCount);
+        if (Maxfood > FoodSpawned)
+        {
+            Instantiate(currentFood, currentBiome.transform.GetChild(randomChild).position + (transform.up * 8.5f), Quaternion.identity);
+            FoodSpawned++;
+        }
 
+
+    }
+    public void PickFood()
+    {
+        // spawnFood = GameObject.FindGameObjectsWithTag("Food");
+        RandomFood = Random.Range(0, spawnFood.Length);
+        currentFood = spawnFood[RandomFood];
+    }
+    public void PickBiome()
+    {
+
+        RandomBiome = Random.Range(0, spawnBiome.Length);
+        currentBiome = spawnBiome[RandomBiome];
+    }
 
 }
 
