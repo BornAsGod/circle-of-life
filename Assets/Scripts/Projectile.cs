c0a16acfd6d584a81275ebeff2e913049f05c14c
shihaya <66997466+shihaya@users.noreply.github.com>
2021-03-25 03:52:38 +0100

    UI setup

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class projectile : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
     Rigidbody rb;
 
