4d74c6f2474e6b1fd93d2525d6df57084ce91669
Valentin Ivanov <37935989+BornAsGod@users.noreply.github.com>
2021-03-26 01:55:14 +0100

    foodSpawne fixed

--- Assets/Scripts/food/foodSpawner.cs
+++ Assets/Scripts/food/foodSpawner.cs
@@ -10,11 +10,7 @@ public class foodSpawner : MonoBehaviour
     public GameManager _GameManager;
 
     public Transform Mid;
-    public Transform grass;
-    public Transform sand;
-    public Transform water;
-    public Transform dirt;
-    public Transform sandstone;
+    
 
     private float offset = 8.5f;
     public int Maxfood;
@@ -26,8 +22,7 @@ public class foodSpawner : MonoBehaviour
     public GameObject[] spawnFood;
     GameObject currentFood;
 
-    public GameObject[] spawnBiome;
-    GameObject currentBiome;
+    
 
     
     // Update is called once per frame
@@ -51,32 +46,14 @@ public class foodSpawner : MonoBehaviour
 
 
     }
-    public void SpawningFoodGrass()
-    {
-        
-        PickFood();
-        PickBiome();
-        int randomChild = Random.Range(0, currentBiome.transform.childCount);
-        if (Maxfood > FoodSpawned)
-        {
-            Instantiate(currentFood, currentBiome.transform.GetChild(randomChild).position + (transform.up * 8.5f), Quaternion.identity);
-            FoodSpawned++;
-        }
-
-
-    }
+   
     public void PickFood()
     {
         // spawnFood = GameObject.FindGameObjectsWithTag("Food");
         RandomFood = Random.Range(0, spawnFood.Length);
         currentFood = spawnFood[RandomFood];
     }
-    public void PickBiome()
-    {
-
-        RandomBiome = Random.Range(0, spawnBiome.Length);
-        currentBiome = spawnBiome[RandomBiome];
-    }
+    
 
 }
 
